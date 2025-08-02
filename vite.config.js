24ce66a5e132b9d2c08673b7e166557686cb6a21
Suriya D'cruze <suryanantakumar0007@gmail.com>
2025-08-02 17:06:11 +0530

    Update vite.config.js
    vite

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-
-
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['fs', 'path'], // add the exact module(s) shown in the error
+    },
+  },
 })
